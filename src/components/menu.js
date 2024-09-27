5d522a0bca65c7f6ccaa60e9efa523c16be66ed0
Doğancan Karakoç <92406871+dogncankrkc@users.noreply.github.com>
2024-09-27 17:06:24 +0300

    Fix image paths for GitHub Pages

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,15 +1,16 @@
 import React from "react";
 
 const images = [
-  "/images/menu_page-0001.webp",
-  "/images/menu_page-0002.webp",
-  "/images/menu_page-0003.webp",
-  "/images/menu_page-0004.webp",
-  "/images/menu_page-0005.webp",
-  "/images/menu_page-0006.webp",
-  "/images/menu_page-0007.webp",
-  "/images/menu_page-0008.webp",
+  `${process.env.PUBLIC_URL}/images/menu_page-0001.webp`,
+  `${process.env.PUBLIC_URL}/images/menu_page-0002.webp`,
+  `${process.env.PUBLIC_URL}/images/menu_page-0003.webp`,
+  `${process.env.PUBLIC_URL}/images/menu_page-0004.webp`,
+  `${process.env.PUBLIC_URL}/images/menu_page-0005.webp`,
+  `${process.env.PUBLIC_URL}/images/menu_page-0006.webp`,
+  `${process.env.PUBLIC_URL}/images/menu_page-0007.webp`,
+  `${process.env.PUBLIC_URL}/images/menu_page-0008.webp`
 ];
+
 
 const Menu = () => {
   return (
